dcd98065a65af47439d8f5d88f3ebec1728933b8
Dalongyi <rhw0213@gmail.com>
2022-02-03 11:44:17 +0900

    스타일 다루기: 아코디언

--- dom.js
+++ dom.js
@@ -1,3 +1,30 @@
+// ---6. 스타일 다루기: 아코디언-------------------------
+
+window.addEventListener("load", function(){
+    var section = document.querySelector("#s6");
+    var box = section.querySelector(".accordion-box");
+    var content = section.querySelectorAll(".accordion-content");
+    var current = section.querySelector(".active");
+    box.onclick=function(e){
+        
+        var valid= e.target.nodeName == "H2" || e.target.classList.contains("accordion-header");
+
+        if(!valid)
+            return;
+
+
+        if(current != null)
+            current.classList.remove("active");
+
+        e.target.classList.toggle("active");
+        current = e.target;
+        
+    }
+    
+
+});
+
+//-------------------------------------------
 //5-1. 숙제) 카드클릭시 이동
 window.addEventListener("load", function(){
     var section = document.querySelector("#s5-1");
