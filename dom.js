333c8719e771175da8af1ebe66ca8aea7710ae5c
Dalongyi <rhw0213@gmail.com>
2022-02-08 11:39:14 +0900

    9

--- dom.js
+++ dom.js
@@ -1,3 +1,43 @@
+window.addEventListener("load",function(){
+    var section = document.querySelector("#s8-1");    
+    var box = section.querySelector(".box");
+
+    box.onclick = function(e){
+        if(e.target.classList.contains("box")){
+            return;
+        }
+    }
+
+    box.onkeydown = function(e){
+        if(e.target.classList.contains("box"))
+            return;
+
+        if(e.code =="Delete"){
+            e.target.classList.add("delete");
+            console.log(e.code);
+        }
+    }
+});
+// --- <h1>8. 이벤트 다루기(focus/blur/tabindex), 스타일(:valid,..) : 입력 값 유효성 검사하기</h1> ----------------------------------
+window.addEventListener("load", function(){
+    var section = document.querySelector("#s8");    
+    var form = section.querySelector("form");
+    var unameInput= form.querySelector("input[name=uname]");
+    form.onsubmit = function(e){
+        e.preventDefault();
+
+        console.log(unameInput.value);
+        if(unameInput.value.length < 3)
+            unameInput.setCustomValidity("이름으로 2자이상");
+
+
+        console.log("submit");
+    };
+
+    //var selectedItem = section.querySelector(".selected");
+});
+
+// 7
 window.addEventListener("load", function(){
     var section = document.querySelector("#s7");
     var box = section.querySelector(".box");
